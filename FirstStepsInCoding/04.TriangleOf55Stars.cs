65fd0161918469e2e6d49dd8df1c90e1e01951ae
mirenci <skitiq0@abv.bg>
2016-10-04 22:41:07 +0300

    problem 4 55 stars trinagle

---
+++ FirstStepsInCoding/04.TriangleOf55Stars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.Triangle_of_55_Stars
+{
+    class TriangleOf55Stars
+    {
+        static void Main()
+        {
+            //foreach row
+            for (int row = 0; row < 10; row++)
+            {
+                //height number of colunms(in current row) are equals of the current row number
+                for (int column = 0; column <= row; column++)
+                {
+                    //put star in each column
+                    Console.Write("*");
+                }
+
+                //start write in new line(row)
+                Console.WriteLine();
+            }
+        }
+    }
+}
