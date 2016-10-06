716b4f7270bb3bedb8ee225b01662f85bc88ffab
mirenci <skitiq0@abv.bg>
2016-10-06 20:42:56 +0300

    06.SquareOfStars.cs

---
+++ FirstStepsInCoding/06.SquareOfStars.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class SquareOfStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int row = 0; row < n; row++)
+        {
+            if (row == 0 || row == n - 1)
+            {
+                for (int column = 0; column < n; column++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int column = 0; column < n; column++)
+                {
+                    if (column == 0 || column == n - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
